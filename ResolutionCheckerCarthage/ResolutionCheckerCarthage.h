83c7d9258f84fc0aa3ca2c71ca64581df758153c
Ta-Cuong Nguyen <cuong@planday.com>
2017-11-13 23:48:46 +0700

    Supported Carthage

---
+++ ResolutionCheckerCarthage/ResolutionCheckerCarthage.h
@@ -0,0 +1,19 @@
+//
+//  ResolutionCheckerCarthage.h
+//  ResolutionCheckerCarthage
+//
+//  Created by admin on 11/13/17.
+//  Copyright © 2017 Ta-Cuong Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ResolutionCheckerCarthage.
+FOUNDATION_EXPORT double ResolutionCheckerCarthageVersionNumber;
+
+//! Project version string for ResolutionCheckerCarthage.
+FOUNDATION_EXPORT const unsigned char ResolutionCheckerCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ResolutionCheckerCarthage/PublicHeader.h>
+
+
